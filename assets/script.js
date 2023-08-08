dc7576332b4efff16ba3d6a33e3e31a0fbf0d568
coterone <coteroneric@gmail.com>
2023-08-07 21:04:42 -0400

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,43 @@
 var countriesUrl = "https://restcountries.com/v3.1/name/";
-var searchInput = document.querySelector('#search-input');
-var countryName = "usa";
+        var form = document.querySelector('form');
+        var searchInput = document.querySelector('#search-input');
+        var displayInfo = document.getElementById('display-info');
 
-fetch(countriesUrl + countryName)
-.then(function (response) {
-return response.json()
-})
-.then(function(data) {
-console.log(data)
-var capital = data[0].capital[0];
-var language = data[0].languages.eng;
-var currency = data[0].currencies.USD.symbol + data[0].currencies.USD.name;
-var population = data[0].population;
-var flag = data[0].flags.png;
-console.log(capital, language, currency, population, flag);
-})
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            var searchTerm = searchInput.value;
+
+            try {
+                var response = await fetch(countriesUrl + searchTerm);
+                var data = await response.json();
+
+                if (data.status === 404) {
+                    displayInfo.innerHTML = "Country not found.";
+                } else {
+                    var countryData = data[0];
+                    var capital = countryData.capital[0];
+                    var language = countryData.languages.eng;
+                    var currency = countryData.currencies.USD.symbol + ' ' + countryData.currencies.USD.name;
+                    var population = countryData.population;
+                    var flag = countryData.flags.png;
+                    var region = data[0].region
+                    var latlng = data[0].latlng
+                    var maps = data[1].maps
+
+                    displayInfo.innerHTML = `
+                        <p>Capital: ${capital}</p>
+                        <p>Language: ${language}</p>
+                        <p>Currency: ${currency}</p>
+                        <p>Population: ${population}</p>
+                        <p>Region: ${region}</p>
+                        <p>Lat-Lng: ${latlng}</p>
+                        <img src="${flag}" alt="Flag" width="100">
+                    `;
+                }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+            console.log(data);
+        });
+
+    
