6b1090d8643ca01e1f45de2bc54905dfb66a6c71
motty mandel <mandelmottyisrael65@gmail.com>
2023-08-08 19:23:56 -0400

    added starter weather function

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,60 @@
 var countriesUrl = "https://restcountries.com/v3.1/name/";
-        var form = document.querySelector('form');
-        var searchInput = document.querySelector('#search-input');
-        var displayInfo = document.getElementById('display-info');
+var form = document.querySelector('form');
+var welcomeMessage = document.querySelector('#earth');
+var searchInput = document.querySelector('#search-input');
+var displayInfo = document.getElementById('display-info');
+var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+var apiKey = "a8a526129b6eee34cf52f1de1b4a6927";
 
-        form.addEventListener('submit', async (event) => {
-            event.preventDefault();
-            var searchTerm = searchInput.value;
 
-            try {
-                var response = await fetch(countriesUrl + searchTerm);
-                var data = await response.json();
 
-                if (data.status === 404) {
-                    displayInfo.innerHTML = "Country not found.";
-                } else {
-                    var countryData = data[0];
-                    var capital = countryData.capital[0];
-                    var language = countryData.languages.eng;
-                    var currency = countryData.currencies.USD.symbol + ' ' + countryData.currencies.USD.name;
-                    var population = countryData.population;
-                    var flag = countryData.flags.png;
-                    var region = data[0].region
-                    var latlng = data[0].latlng
-                    var maps = data[1].maps
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    welcomeMessage.style.display = "none";
+    var searchTerm = searchInput.value;
 
-                    displayInfo.innerHTML = `
-                        <p>Capital: ${capital}</p>
-                        <p>Language: ${language}</p>
-                        <p>Currency: ${currency}</p>
-                        <p>Population: ${population}</p>
-                        <p>Region: ${region}</p>
-                        <p>Lat-Lng: ${latlng}</p>
-                        <img src="${flag}" alt="Flag" width="100">
-                    `;
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
+    try {
+        var response = await fetch(countriesUrl + searchTerm);
+        var data = await response.json();
+
+        if (data.status === 404) {
+            displayInfo.innerHTML = "Country not found.";
+        } else {
+            var countryData = data[0];
+            var capital = countryData.capital[0];
+            var language = countryData.languages.eng;
+            var currency = countryData.currencies.USD.symbol + ' ' + countryData.currencies.USD.name;
+            var population = countryData.population;
+            var flag = countryData.flags.png;
+            var region = data[0].region
+            var latlng = data[0].latlng
+            var maps = data[1].maps
+
+            var fetchWeather = function () {
+                fetch(weatherUrl + searchInput.value + "&appid=" + apiKey + "&units=imperial")
+                    .then(function (response) {
+                        return response.json()
+                    })
+                    .then(function (weatherData) {
+                        console.log(weatherData);
+                    })
             }
-            console.log(data);
-        });
 
-    
+
+            displayInfo.innerHTML = `
+                    <p>Capital: ${capital}</p>
+                    <p>Language: ${language}</p>
+                    <p>Currency: ${currency}</p>
+                    <p>Population: ${population}</p>
+                    <p>Region: ${region}</p>
+                    <p>Lat-Lng: ${latlng}</p>
+                    <img src="${flag}" alt="Flag" width="100">
+                    `;
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+    console.log(data);
+});
+
+
