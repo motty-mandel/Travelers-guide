8f853c65d419fdb87be654e7701b4ee8b852eb4a
2dogsandaguy <Neverclear@ymail.com>
2023-08-15 20:36:35 -0400

    polish up the code and made past searches not show

--- assets/script.js
+++ assets/script.js
@@ -17,12 +17,13 @@ var selectedOptions = JSON.parse(localStorage.getItem('selectedOptions')) || [];
 var selectedCountries = JSON.parse(localStorage.getItem('selectedCountries')) || [];
 // Retrieve the combined array from localStorage
 var combinedArray = JSON.parse(localStorage.getItem('combinedArray')) || [];
-
 // Retrieve and filter the selected options from the combined array
 var selectedOptionsFromCombined = combinedArray.filter(item => !item.capital);
-
 // Display the list of selected countries and options
 displaySelectedItems(selectedOptionsFromCombined);
+window.addEventListener('load', displaySelectedItems);
+
+setupClearButton();
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
@@ -32,153 +33,140 @@ form.addEventListener('submit', async (event) => {
   var searchTerm = searchInput.value;
 
   try {
-      var response = await fetch(countriesUrl + searchTerm);
-      var data = await response.json();
-
-      console.log(data);
-
-      if (response.status === 404) {
-          displayInfo.innerHTML = "Country not found.";
+    var response = await fetch(countriesUrl + searchTerm);
+    var data = await response.json();
+    console.log(data);
+    if (response.status === 404) {
+      displayInfo.innerHTML = "Country not found.";
+    } else {
+      if (data.length === 1) {
+        var countryData = data[0];
+        displayCountryInfo(countryData);
+        // Add the country info to the array of selected countries
+        selectedCountries.push({
+          capital: countryData.name.common,
+          name: countryData.capital[0],
+          region: countryData.region
+        });
+        // Combine selectedOptions and selectedCountries arrays
+        combinedArray = [...selectedOptions, ...selectedCountries];
+        // Update local storage with the updated array
+        localStorage.setItem('selectedCountries', JSON.stringify(selectedCountries));
+        localStorage.setItem('combinedArray', JSON.stringify(combinedArray));
       } else {
-          if (data.length === 1) {
-              var countryData = data[0];
-              displayCountryInfo(countryData);
-
-              // Add the country info to the array of selected countries
-              selectedCountries.push({
-                  capital: countryData.name.common,
-                  name: countryData.capital[0],
-                  region: countryData.region
-              });
-
-              // Combine selectedOptions and selectedCountries arrays
-              combinedArray = [...selectedOptions, ...selectedCountries];
-
-              // Update local storage with the updated array
-              localStorage.setItem('selectedCountries', JSON.stringify(selectedCountries));
-              localStorage.setItem('combinedArray', JSON.stringify(combinedArray));
-          } else {
-              displayCountryOptions(data);
-              
-          }
+        displayCountryOptions(data);
       }
+    };
   } catch (error) {
-      console.error("Error fetching data:", error);
-  }
+    console.error("Error fetching data:", error);
+  };
 });
 
 
 function displayCountryOptions(countries) {
-    weatherInfo.style.display = "none";
-    var optionsHtml = countries.map((country, index) => {
-        return `<option value="${index}">${country.name.common}</option>`;
-    }).join('');
+  weatherInfo.style.display = "none";
+  var optionsHtml = countries.map((country, index) => {
+    return `<option value="${index}">${country.name.common}</option>`;
+  }).join('');
 
-    displayInfo.innerHTML = `
+  displayInfo.innerHTML = `
         <p>Multiple countries found. Please select one:</p>
         <select id="country-select">
             ${optionsHtml}
         </select>
-        <button id="select-button">Select</button>
-    `;
+        <button id="select-button">Select</button> `;
 
-    var selectButton = document.getElementById('select-button');
-    var countrySelect = document.getElementById('country-select');
+  var selectButton = document.getElementById('select-button');
+  var countrySelect = document.getElementById('country-select');
 
-    selectButton.addEventListener('click', () => {
-      var selectedIndex = parseInt(countrySelect.value);
-      var selectedCountry = countries[selectedIndex];
-      displayCountryInfo(selectedCountry);
-      weatherInfo.style.display = "block";
-  
-      selectedOptions.push({
-          capital: selectedCountry.name.common,
-          name: selectedCountry.capital[0],
-          region: selectedCountry.region
-      });
-  
-      // Combine selectedOptions and selectedCountries arrays
-      combinedArray = [...selectedOptions, ...selectedCountries];
-  
-      // Update local storage with the updated array
-      localStorage.setItem('selectedOptions', JSON.stringify(selectedOptions));
-      localStorage.setItem('combinedArray', JSON.stringify(combinedArray));
+  selectButton.addEventListener('click', () => {
+    var selectedIndex = parseInt(countrySelect.value);
+    var selectedCountry = countries[selectedIndex];
+    displayCountryInfo(selectedCountry);
+    weatherInfo.style.display = "block";
+
+    selectedOptions.push({
+      capital: selectedCountry.name.common,
+      name: selectedCountry.capital[0],
+      region: selectedCountry.region
     });
-}
+    // Combine selectedOptions and selectedCountries arrays
+    combinedArray = [...selectedOptions, ...selectedCountries];
+    // Update local storage with the updated array
+    localStorage.setItem('selectedOptions', JSON.stringify(selectedOptions));
+    localStorage.setItem('combinedArray', JSON.stringify(combinedArray));
+  });
+};
 
 function displayCountryInfo(countryData) {
-    try {
-        var capital = countryData.capital[0];
-        var languages = Object.values(countryData.languages).join(', ');
-        var currencySymbol, currencyName;
-        for (var currencyCode in countryData.currencies) {
-            currencySymbol = countryData.currencies[currencyCode].symbol;
-            currencyName = countryData.currencies[currencyCode].name;
-            break;
-        }
-        var population = countryData.population;
-        var flag = countryData.flags.png;
-        var region = countryData.region;
-        var latlng = countryData.latlng;
-
-        var fetchWeather = function () {
-            fetch(weatherUrl + capital + "&appid=" + apiKey + "&units=imperial")
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    console.log( 'fetch-weather', data);
-                  // Display the list of selected countries
-                  displaySelectedItems();
-                    var temp = data.main.temp;
-                    var forecast = data.weather[0].description;
-                    var icon = data.weather[0].icon;
-                    var humidity = data.main.humidity;
-                    var capitol = data.name
- 
-
-                    weatherInfo.innerHTML = ` 
-                        <img class="weather-img" src="${iconUrl + icon + ".png"}" alt="weather-icon">
-                        <p class="name-captiol"><strong>Weather For:</strong> ${capitol}
-                        <p class="forecast-info"><strong>Forecast: </strong> ${forecast}</p>
-                        <p class="temp-info"><strong>Temperature:</strong> ${temp}°F</p>
-                        <p class="humidity"><strong>Humidity:</strong> ${humidity}%</p>
-                        <img  src="" alt="">
-                    `;
-
-                });
-
-        };
-        fetchWeather();
-
-        displayInfo.innerHTML = `
-            <p class="capital-info"><strong>Capital:</strong> ${capital}</p>
-            <p class="language-info"><strong>Language: </strong> ${languages}</p>
-            <p class="currency-info"><strong>Currency: </strong> ${currencySymbol} ${currencyName}</p>
-            <p class="population-info"><strong>Population:</strong> ${population}</p>
-            <p class="region-info"><strong>Region: </strong> ${region}</p>
-            <p class="latlng-info"><strong>Lat-Lng: </strong> <a href="https://www.openstreetmap.org/#map=4/${latlng[0]}/${latlng[1]}" target="_blank">${latlng[0]}, ${latlng[1]}</a></p>
-            <img class="flag-info" src="${flag}" alt="Flag" width="100">
-        `;
-        // Display the list of selected countries
-        displaySelectedItems();
-    } catch (error) {
-        console.error("Error displaying country info:", error);
+  try {
+    var capital = countryData.capital[0];
+    var languages = Object.values(countryData.languages).join(', ');
+    var currencySymbol, currencyName;
+    for (var currencyCode in countryData.currencies) {
+      currencySymbol = countryData.currencies[currencyCode].symbol;
+      currencyName = countryData.currencies[currencyCode].name;
+      break;
     }
-}
+    var population = countryData.population;
+    var flag = countryData.flags.png;
+    var region = countryData.region;
+    var latlng = countryData.latlng;
+
+    var fetchWeather = function () {
+      fetch(weatherUrl + capital + "&appid=" + apiKey + "&units=imperial")
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log('fetch-weather', data);
+          // Display the list of selected countries
+          displaySelectedItems();
+          var temp = data.main.temp;
+          var forecast = data.weather[0].description;
+          var icon = data.weather[0].icon;
+          var humidity = data.main.humidity;
+          var capitol = data.name;
 
 
+          weatherInfo.innerHTML = ` 
+                        <img class="the-info" src="${iconUrl + icon + ".png"}" alt="weather-icon">
+                        <p class="the-info"><strong>Weather For:</strong> ${capitol}
+                        <p class="the-info"><strong>Forecast: </strong> ${forecast}</p>
+                        <p class="the-info"><strong>Temperature:</strong> ${temp}°F</p>
+                        <p class="the-info"><strong>Humidity:</strong> ${humidity}%</p>
+                        <img  src="" alt="">`;
+        });
+    };
+    fetchWeather();
+
+    displayInfo.innerHTML = `
+            <p class="the-info"><strong>Capital:</strong> ${capital}</p>
+            <p class="the-info"><strong>Language: </strong> ${languages}</p>
+            <p class="the-info"><strong>Currency: </strong> ${currencySymbol} ${currencyName}</p>
+            <p class="the-info"><strong>Population:</strong> ${population}</p>
+            <p class="the-info"><strong>Region: </strong> ${region}</p>
+            <p class="the-info"><strong>Lat-Lng: </strong> <a href="https://www.openstreetmap.org/#map=4/${latlng[0]}/${latlng[1]}" target="_blank">${latlng[0]}, ${latlng[1]}</a></p>
+            <img class="the-info" src="${flag}" alt="Flag" width="100"> `;
+    // Display the list of selected countries
+    displaySelectedItems();
+  } catch (error) {
+    console.error("Error displaying country info:", error);
+  };
+};
 // Display the list of selected countries and options
 function displaySelectedItems() {
   var selectedItemsList = document.getElementById('selected-items-list');
   selectedItemsList.innerHTML = "";
-
   // Reverse the combinedArray to display it in the correct order
   var reversedArray = combinedArray.slice().reverse();
-
   // Limit to a maximum of 10 items
   var itemsToDisplayLimited = reversedArray.slice(0, 10);
-
+  if (combinedArray.length > 0) {
+    document.getElementById('lasted-searched').style.display = 'block'; // Show the section
+  } else {
+    document.getElementById('lasted-searched').style.display = 'none'; // Hide the section
+  }
   itemsToDisplayLimited.forEach(function (item) {
     var itemText = item.capital + ", " + item.name + " (" + item.region + ")";
     var listItem = document.createElement('li');
@@ -189,27 +177,24 @@ function displaySelectedItems() {
       // Handle button click event for items 
       // Populate the search input field with the country name
       searchInput.value = item.capital;
-      
     });
     listItem.appendChild(itemButton);
     selectedItemsList.appendChild(listItem);
   });
-}
+};
 
 function setupClearButton() {
   var clearButton = document.getElementById('clear-button');
   if (localStorage.length > 0) {
-      clearButton.style.display = "block";
+    clearButton.style.display = "block";
   } else {
-      clearButton.style.display = "none";
-  } 
+    clearButton.style.display = "none";
+  }
 
-  clearButton.addEventListener('click', function() {
+  clearButton.addEventListener('click', function () {
     localStorage.clear();
     location.reload();
-    /* var element = document.getElementById('selected-items-list');
-       element.remove(); */
   });
-}
-setupClearButton()
-  window.addEventListener('load', displaySelectedItems);
+};
+
+
